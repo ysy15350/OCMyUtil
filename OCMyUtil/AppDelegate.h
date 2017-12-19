58ad9d3066d097a7b6cd2bfe9d16a85de4cc21bd
yangshiyou <ysy15350@gmail.com>
2017-12-19 19:18:33 +0800

    Initial Commit

---
+++ OCMyUtil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCMyUtil
+//
+//  Created by 杨世友 on 2017/12/19.
+//  Copyright © 2017年 ysy15350. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCMyUtil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCMyUtil
+//
+//  Created by 杨世友 on 2017/12/19.
+//  Copyright © 2017年 ysy15350. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
